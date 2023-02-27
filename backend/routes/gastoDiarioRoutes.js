3ae252068b7e2b095481a5f01c17c8569c8045a8
FraneSpralja <spralja.frane@gmail.com>
2023-02-26 23:32:37 -0300

    request - update - delete Gastos con autenticación de  usuario

--- backend/routes/gastoDiarioRoutes.js
+++ backend/routes/gastoDiarioRoutes.js
@@ -2,14 +2,24 @@ import express from 'express';
 
 import { 
     agregarGasto,
-    obtenerGasto 
+    obtenerGastos,
+    obtenerGasto,
+    actualizarGasto,
+    eliminarGasto
 } from '../controllers/gastoDiarioControllers.js';
 import checkAuth from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
+// Toas las rutas son privadas y necesitan auth
+
 router.route('/')
     .post(checkAuth, agregarGasto)
-    .get(checkAuth, obtenerGasto)
+    .get(checkAuth, obtenerGastos);
 
+router
+    .route('/:id')
+    .get(checkAuth, obtenerGasto)
+    .put(checkAuth, actualizarGasto)
+    .delete(checkAuth, eliminarGasto)
 export default router;
