c2c8b7cf19a17c66c272dc114288b233c42f17da
FraneSpralja <spralja.frane@gmail.com>
2023-03-01 23:47:42 -0300

    test con style scoped para personalización mediante css # Construcción de pagina Login

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom"
 
+// Styles
+import styles from "../assets/style/authLayout.module.css"
+
 const AuthLayout = () => {
     return (
         <>
             <h1>Desde AuthLayout</h1>
             
-            <Outlet />
+            <main className={styles.mainContainer}>
+                <Outlet />
+            </main>
         </>
     )
 }
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,7 +1,66 @@
+import { Link } from "react-router-dom"
+// Style
+import styles from "../assets/style/authLayout.module.css"
+
 const Login = () => {
     return (
         <>
-            <h2>Desde Login</h2>
+                <div className={styles.loginHeader}>
+                    <h1 
+                    className={styles.loginHeader_title}
+                        >Bienvenido a <span>Ahorrapp</span>, <br /> 
+                        inicia sesión y empieza a ahorrar
+                    </h1>
+                </div>
+                <div className={styles.loginBody}>
+                    <form action="">
+                        <div className={styles.formBox}>
+                            <label 
+                            className={styles.formBox_label}
+                            >
+                                Email <span className="text-red-300">*</span>
+                            </label>
+                            <input
+                            className={styles.formBox_input}
+                            type="text" 
+                            placeholder="usuario@correo.com"
+                            />
+                        </div>
+                        <div className={styles.formBox}>
+                            <label 
+                            className={styles.formBox_label}
+                            >
+                                Password <span className="text-red-300">*</span>
+                            </label>
+                            <input
+                            className={styles.formBox_input}
+                            type="password" 
+                            placeholder="Tu password"
+                            />
+                        </div>
+                        <div className={styles.formBox}>
+                            <input 
+                            type="submit" 
+                            value="Iniciar Sesión"
+                            className={styles.btnSubmit}
+                            />
+                        </div>
+                        <div className={styles.formBox}>
+                            <Link 
+                            className={styles.links_submit} 
+                            to="/registrar"
+                            >
+                                Únete a los que ahorran, regístrate
+                            </Link>
+                            <Link 
+                            className={styles.links_submit} 
+                            to="/olvide-password"
+                            >
+                                Recupera tu password
+                            </Link>
+                        </div>
+                    </form>
+                </div>
         </>
     )
 }
